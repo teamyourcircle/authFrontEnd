3e1743b933c43cde56fa12447c53627ac50ed35c
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2021-01-29 21:56:22 +0530

    google oauth done frontend

--- src/Google_Login.js
+++ src/Google_Login.js
@@ -3,66 +3,38 @@ import Cookies from 'js-cookie';
 import { GoogleLogin } from 'react-google-login';
 import {AuthContext} from './AuthContext';
 
+const clientId ='391383527608-vq5pjfpslfeq4i10624rvt088eqhsa4p.apps.googleusercontent.com';
 
-// refresh token
-// import { refreshTokenSetup } from '../utils/refreshToken';
-
-const clientId =
-  '391383527608-vq5pjfpslfeq4i10624rvt088eqhsa4p.apps.googleusercontent.com';
-//
-
-
-//
 function Login() {
 
-    const [isAuth,setAuth] = useContext(AuthContext);
+  const [isAuth,setAuth] = useContext(AuthContext);
   const onSuccess = (res) => {
-    var oauth2Client = getOAuthClient();
-    console.log("kbkbkbkb")
-      
-      console.log("Response",res)
-    // let token_Id = res.tokenId;
-
-    console.log(token_Id)
-      //
-      fetch('http://localhost:5000/auth/api/socialLogin', {
-        method: 'POST',
-        headers: {'Content-Type':'application/json'},
-        body:JSON.stringify({
-            "access_token":`${token_Id}`,
-            "social_type":"google"
-        })
-              
-      }).then(function(res) {
-          if (!res.ok) {
-              throw Error(res.statusText);
-          }
-          return res;
-      }).then(async function(res) {
-        const data = await res.json();
-        console.log("Data",data)
-        Cookies.set("Token", data.token);
-        Cookies.set("isAuth",isAuth);
-        window.open('/dashboard', "_self")
-      }).catch(function(error) {
-          console.log(error);
-      });
-      
-
-
-      
-    console.log('Login Success: currentUser:', res.profileObj);
-    // alert(
-    //   `Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`
-    // );
-    // refreshTokenSetup(res);
-  };
-
+  let token_Id = res.tokenId;
+  fetch('http://localhost:5000/auth/api/socialLogin', {
+    method: 'POST',
+    headers: {'Content-Type':'application/json'},
+    body:JSON.stringify({
+        "access_token":`${token_Id}`,
+        "social_type":"google"
+    })
+            
+    }).then(function(res) {
+      if (!res.ok) {
+          throw Error(res.statusText);
+      }
+      return res;
+    }).then(async function(res) {
+      const data = await res.json();
+      console.log("Data",data)
+      Cookies.set("Token", data.token);
+      Cookies.set("isAuth",isAuth);
+      window.open('/dashboard', "_self")
+    }).catch(function(error) {
+      console.log(error);
+    });
+    }
   const onFailure = (res) => {
     console.log('Login failed: res:', res);
-    // alert(
-    //   `Failed to login. 😢 Please ping this to repo owner twitter.com/sivanesh_fiz`
-    // );
   };
 
   return (
@@ -74,8 +46,6 @@ function Login() {
         onFailure={onFailure}
         cookiePolicy={'single_host_origin'}
         style={{ marginTop: '100px' }}
-        // isSignedIn={false}
-        responseType = {'code'}
       />
     </div>
   );
